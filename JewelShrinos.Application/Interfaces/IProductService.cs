b8f48c18ec37f9879528b6cef9f9a5981530a574
Manu9099 <manucht51@gmail.com>
2026-04-08 18:57:40 -0500

    fix program

--- JewelShrinos.Application/Interfaces/IProductService.cs
+++ JewelShrinos.Application/Interfaces/IProductService.cs
@@ -1,19 +1,19 @@
-using JewelShrinos.Core.Entities;
-using System.Linq.Expressions;
+// JewelShrinos.Application/Interfaces/IProductService.cs
 using JewelShrinos.Application.DTOs.Request.Product;
-    
-    public interface IProductService
-    {
-        Task<ProductResponse?> GetByIdAsync(int id);
-        Task<IEnumerable<ProductResponse>> GetAllAsync();
-        Task<ProductResponse?> GetByCodeAsync(string code);
-        Task<ProductResponse?> GetByBarcodeAsync(string barcode);
-        Task<ProductResponse?> GetByQrCodeAsync(string qrCode);
-        Task<IEnumerable<ProductResponse>> GetByCategoryAsync(int categoryId);
-        Task<IEnumerable<ProductResponse>> GetByMaterialAsync(int materialId);
-        
-        Task<ProductResponse> CreateAsync(CreateProductRequest request);
-        Task<ProductResponse> UpdateAsync(int id, UpdateProductRequest request);
-        Task<bool> DeleteAsync(int id);
-    }
+using JewelShrinos.Application.DTOs.Response.Product;
 
+namespace JewelShrinos.Application.Interfaces;
+
+public interface IProductService
+{
+    Task<ProductResponse?> GetByIdAsync(int id);
+    Task<IEnumerable<ProductResponse>> GetAllAsync();
+    Task<ProductResponse?> GetByCodeAsync(string code);
+    Task<ProductResponse?> GetByBarcodeAsync(string barcode);
+    Task<ProductResponse?> GetByQrCodeAsync(string qrCode);
+    Task<IEnumerable<ProductResponse>> GetByCategoryAsync(int categoryId);
+    Task<IEnumerable<ProductResponse>> GetByMaterialAsync(int materialId);
+    Task<ProductResponse> CreateAsync(CreateProductRequest request);
+    Task<ProductResponse> UpdateAsync(int id, UpdateProductRequest request);
+    Task<bool> DeleteAsync(int id);
+}
